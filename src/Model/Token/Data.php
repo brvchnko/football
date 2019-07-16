d045c8d751ea9204006757160be367bd49f2ca6a
Taras Brovchenko <brovchenko7@gmail.com>
2019-07-16 20:13:21 +0300

    update token layers

--- src/Model/Token/Data.php
+++ src/Model/Token/Data.php
@@ -7,36 +7,12 @@ namespace App\Model\Token;
 class Data
 {
     /**
-     * @var Meta|null
-     */
-    private $meta;
-
-    /**
      * @var User|null
      */
-    private $user;
+    public $user;
 
-    public function getMeta(): ?Meta
-    {
-        return $this->meta;
-    }
-
-    public function setMeta(?Meta $meta): self
-    {
-        $this->meta = $meta;
-
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
-
-        return $this;
-    }
+    /**
+     * @var Meta|null
+     */
+    public $meta;
 }
