d7880c69c3e67b45fb64f9785f842c05bac569ce
Taras Brovchenko <brovchenko7@gmail.com>
2019-07-17 02:57:05 +0300

    Add token factory tests

---
+++ tests/DataFixtures/Model/Token/TokenData.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\DataFixtures\Model\Token;
+
+use App\Entity\User;
+use App\Model\Token as TokenModel;
+
+class TokenData
+{
+    public static function get(User $user, string $tokenId, int $tokenExpiry): TokenModel\Data
+    {
+        $dataModel = new TokenModel\Data();
+
+        $metaModel = new TokenModel\Meta();
+        $metaModel->id = $tokenId;
+        $metaModel->expiry = $tokenExpiry;
+
+        $dataModel->meta = $metaModel;
+
+        $userModel = new TokenModel\User();
+        $userModel->id = $user->getId();
+        $userModel->email = $user->getEmail();
+        $userModel->roles = $user->getRoles();
+
+
+        $dataModel->user = $userModel;
+
+        return $dataModel;
+    }
+}
