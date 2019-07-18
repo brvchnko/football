db4527cb6b5b121007b0bf09719375d7e110c914
Taras Brovchenko <brovchenko7@gmail.com>
2019-07-18 14:24:33 +0300

    Update TokenFactory

--- src/Factory/TokenFactory.php
+++ src/Factory/TokenFactory.php
@@ -4,26 +4,26 @@ declare(strict_types=1);
 
 namespace App\Factory;
 
-use App\Model\Token as TokenModel;
 use App\Entity\User;
+use App\Model\Token\Data;
+use App\Model\Token\Meta;
+use App\Model\Token\User as UserModel;
 
 class TokenFactory
 {
-    public function create(User $user, string $tokenId, int $tokenExpiry): TokenModel\Data
+    public function create(User $user, string $tokenId, int $tokenExpiry): Data
     {
-        $dataModel = new TokenModel\Data();
-
-        $metaModel = new TokenModel\Meta();
+        $metaModel = new Meta();
         $metaModel->id = $tokenId;
         $metaModel->expiry = $tokenExpiry;
 
-        $dataModel->meta = $metaModel;
-
-        $userModel = new TokenModel\User();
+        $userModel = new UserModel();
         $userModel->id = $user->getId();
         $userModel->email = $user->getEmail();
         $userModel->roles = $user->getRoles();
 
+        $dataModel = new Data();
+        $dataModel->meta = $metaModel;
         $dataModel->user = $userModel;
 
         return $dataModel;
